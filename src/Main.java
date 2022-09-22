dbc788879ee3a2583ff2a8d6ef2d42e3c3c7f5ce
arleenmonteiro <monteiak@mail.uc.edu>
2022-09-22 13:39:40 -0400

    work done!

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.print("Enter your age: ");
+        int age=34;
+        System.out.println(age);
+        if (age>=21)
+        {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
